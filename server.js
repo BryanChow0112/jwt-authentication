11ef0b5466ea3281e9950a5db064b3c85ed418b1
BryanChow0112 <120325403+BryanChow0112@users.noreply.github.com>
2025-01-28 11:26:27 +0800

    Implement JWT authentication for posts retrieval

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
 const express = require("express");
+const jwt = require("jsonwebtoken");
 const app = express();
+
+app.use(express.json());
 
 const posts = [
   { username: "Bryan", title: "Post 1" },
@@ -8,9 +11,23 @@ const posts = [
   { username: "Edward", title: "Post 3" }
 ];
 
-app.get("/posts", (req, res) => {
-  res.json(posts);
+app.get("/posts", authenticateToken, (req, res) => {
+  res.json(posts.filter((post) => post.username === req.user.name));
 });
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+
+    req.user = user;
+    next();
+  });
+}
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
